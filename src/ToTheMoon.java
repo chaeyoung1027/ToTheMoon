adeeaa405292c79d71abc4dfd27366b44bbf93f6
chaeyoung1027 <s2111@e-mirim.hs.kr>
2023-05-14 22:18:05 +0900

    중간커밋

--- src/ToTheMoon.java
+++ src/ToTheMoon.java
@@ -14,34 +14,48 @@ import javax.swing.JPanel;
 
 public class ToTheMoon extends JFrame {
     private Image mainBackground;
+
     private ImageIcon originalStartButtonIcon;
+    private ImageIcon originalRuleButtonIcon;
+    private ImageIcon originalExitButtonIcon;
+
     private ImageIcon scaledStartButtonIcon;
-    private JButton startButton;
+    private ImageIcon scaledRuleButtonIcon;
+    private ImageIcon scaledExitButtonIcon;
+
+    private JButton StartButton;
+    private JButton RuleButton;
+    private JButton ExitButton;
 
     public ToTheMoon() {
         mainBackground = new ImageIcon(getClass().getResource("img/MainBackground.png")).getImage();
         originalStartButtonIcon = new ImageIcon(getClass().getResource("img/StartButton.png"));
+        originalRuleButtonIcon = new ImageIcon(getClass().getResource("img/RuleButton.png"));
+        originalExitButtonIcon = new ImageIcon(getClass().getResource("img/ExitButton.png"));
 
         // 초기 이미지 크기 설정
         int buttonWidth = originalStartButtonIcon.getIconWidth();
         int buttonHeight = originalStartButtonIcon.getIconHeight();
 
-        // 이미지 크기를 90%로 축소
-        int scaledWidth = (int) (buttonWidth * 0.6);
-        int scaledHeight = (int) (buttonHeight * 0.6);
+        // 이미지 크기를 원본의 60%로 축소
+        int scaledWidth = (int) (buttonWidth * 0.5);
+        int scaledHeight = (int) (buttonHeight * 0.5);
 
         // 이미지 크기 조정
         scaledStartButtonIcon = new ImageIcon(originalStartButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
+        scaledRuleButtonIcon = new ImageIcon(originalRuleButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
+        scaledExitButtonIcon = new ImageIcon(originalExitButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
 
-        startButton = new JButton(scaledStartButtonIcon);
-        startButton.setBorderPainted(false);
-        startButton.setContentAreaFilled(false);
-        startButton.setFocusPainted(false);
-        startButton.setOpaque(false);
-        startButton.setBounds(782, 320, scaledWidth, scaledHeight);
+        //startButton 설정
+        StartButton = new JButton(scaledStartButtonIcon);
+        StartButton.setBorderPainted(false);
+        StartButton.setContentAreaFilled(false);
+        StartButton.setFocusPainted(false);
+        StartButton.setOpaque(false);
+        StartButton.setBounds(782, 320, scaledWidth, scaledHeight);
 
-        //마우스
-        startButton.addMouseListener(new MouseAdapter() {
+        //startButton 마우스 리스너
+        StartButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 // 이미지 크기를 90%로 축소
@@ -51,14 +65,73 @@ public class ToTheMoon extends JFrame {
                 // 이미지 크기 조정
                 scaledStartButtonIcon = new ImageIcon(originalStartButtonIcon.getImage().getScaledInstance(pressedWidth, pressedHeight, Image.SCALE_SMOOTH));
 
-                startButton.setIcon(scaledStartButtonIcon);
+                StartButton.setIcon(scaledStartButtonIcon);
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
                 scaledStartButtonIcon = new ImageIcon(originalStartButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
 
-                startButton.setIcon(scaledStartButtonIcon);
+                StartButton.setIcon(scaledStartButtonIcon);
+            }
+        });
+
+        //RuleButton 설정
+        RuleButton = new JButton(scaledRuleButtonIcon);
+        RuleButton.setBorderPainted(false);
+        RuleButton.setContentAreaFilled(false);
+        RuleButton.setFocusPainted(false);
+        RuleButton.setOpaque(false);
+        RuleButton.setBounds(782, 400, scaledWidth, scaledHeight);
+
+        //RuleButton 마우스 리스너
+        RuleButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // 이미지 크기를 90%로 축소
+                int pressedWidth = (int) (scaledWidth * 0.9);
+                int pressedHeight = (int) (scaledHeight * 0.9);
+
+                // 이미지 크기 조정
+                scaledRuleButtonIcon = new ImageIcon(originalRuleButtonIcon.getImage().getScaledInstance(pressedWidth, pressedHeight, Image.SCALE_SMOOTH));
+
+                RuleButton.setIcon(scaledRuleButtonIcon);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                scaledRuleButtonIcon = new ImageIcon(originalRuleButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
+
+                RuleButton.setIcon(scaledRuleButtonIcon);
+            }
+        });
+
+        //ExitButton 설정
+        ExitButton = new JButton(scaledExitButtonIcon);
+        ExitButton.setBorderPainted(false);
+        ExitButton.setContentAreaFilled(false);
+        ExitButton.setFocusPainted(false);
+        ExitButton.setOpaque(false);
+        ExitButton.setBounds(782, 480, scaledWidth, scaledHeight);
+        //ExitButton 마우스 리스너
+        ExitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // 이미지 크기를 90%로 축소
+                int pressedWidth = (int) (scaledWidth * 0.9);
+                int pressedHeight = (int) (scaledHeight * 0.9);
+
+                // 이미지 크기 조정
+                scaledExitButtonIcon = new ImageIcon(scaledExitButtonIcon.getImage().getScaledInstance(pressedWidth, pressedHeight, Image.SCALE_SMOOTH));
+
+                ExitButton.setIcon(scaledExitButtonIcon);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                scaledExitButtonIcon = new ImageIcon(originalExitButtonIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH));
+
+                ExitButton.setIcon(scaledExitButtonIcon);
             }
         });
 
@@ -71,7 +144,10 @@ public class ToTheMoon extends JFrame {
         };
         contentPane.setLayout(null);
         contentPane.setOpaque(false);
-        contentPane.add(startButton);
+        //버튼 추가
+        contentPane.add(StartButton);
+        contentPane.add(RuleButton);
+        contentPane.add(ExitButton);
 
         setCursor(Cursor.getDefaultCursor());
         setResizable(false);
