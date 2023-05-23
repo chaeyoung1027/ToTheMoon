8dc237a34c76a5e8a2f7966982e053bfe8354ad1
chaeyoung1027 <rnldyal1027@naver.com>
2023-05-23 09:42:25 +0900

    토끼 움직임 수정ing

--- src/EarthPart.java
+++ src/EarthPart.java
@@ -6,9 +6,12 @@ import java.awt.event.KeyListener;
 public class EarthPart extends JFrame implements KeyListener {
     private Image earthBackground;
     private ImageIcon[] rabbit = new ImageIcon[2];
-    private ImageIcon rabbitBefore;
-    private ImageIcon rabbitJumpBefore;
-    private boolean isJumping = false; // 토끼가 점프 중인지 여부를 나타내는 플래그
+    private boolean isJumping = false;
+    private int rabbitX = 500;
+    private int rabbitY = 500;
+    private int rabbitMoveSpeed = 200; // Rabbit movement speed (pixels per second)
+    private Timer timer;
+    private long lastKeyPressTime = 0;
 
     public EarthPart() {
         setUndecorated(true);
@@ -16,11 +19,11 @@ public class EarthPart extends JFrame implements KeyListener {
         setLocationRelativeTo(null);
         setBackground(new Color(0, 0, 0, 0));
         setLayout(null);
+        setFocusable(true); // Set focusable to receive key events
 
-        // 이미지 지정
         earthBackground = new ImageIcon(getClass().getResource("img/EarthBackground.png")).getImage();
-        rabbitBefore = new ImageIcon(getClass().getResource("img/rabbit.png"));
-        rabbitJumpBefore = new ImageIcon(getClass().getResource("img/rabbitJump.png"));
+        ImageIcon rabbitBefore = new ImageIcon(getClass().getResource("img/rabbit.png"));
+        ImageIcon rabbitJumpBefore = new ImageIcon(getClass().getResource("img/rabbitJump.png"));
 
         int rabbitWidth = rabbitBefore.getIconWidth();
         int rabbitHeight = rabbitBefore.getIconHeight();
@@ -36,14 +39,50 @@ public class EarthPart extends JFrame implements KeyListener {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(earthBackground, 0, 0, getWidth(), getHeight(), null);
-                g.drawImage(rabbit[isJumping ? 1 : 0].getImage(), 500, 500, null);  // x, y는 토끼 이미지의 위치입니다.
+                g.drawImage(rabbit[isJumping ? 1 : 0].getImage(), rabbitX, rabbitY, null);
             }
         };
 
         contentPane.setLayout(null);
-        setContentPane(contentPane);  // JPanel을 프레임의 컨텐트 팬으로 설정
+        setContentPane(contentPane);
         setVisible(true);
         addKeyListener(this);
+
+        timer = new Timer(16, e -> {
+            long currentTime = System.currentTimeMillis();
+            long elapsedTime = currentTime - lastKeyPressTime;
+
+            if (isJumping) {
+                rabbitY -= 2;
+                if (rabbitY <= 400) {
+                    isJumping = false;
+                }
+            } else {
+                rabbitY += 2;
+                if (rabbitY >= 500) {
+                    rabbitY = 500;
+                    timer.stop();
+                }
+            }
+
+            if (elapsedTime >= 16) { // Adjust the interval if needed for smoother movement
+                double movementDelta = rabbitMoveSpeed * (elapsedTime / 1000.0); // Calculate the position change based on elapsed time
+
+                if (KeyState.isRightPressed) {
+                    rabbitX += movementDelta;
+                    if (rabbitX >= 1420) { // Adjust the right boundary if needed
+                        rabbitX = 1420;
+                    }
+                } else if (KeyState.isLeftPressed) {
+                    rabbitX -= movementDelta;
+                    if (rabbitX <= 20) { // Adjust the left boundary if needed
+                        rabbitX = 20;
+                    }
+                }
+                contentPane.repaint();
+                lastKeyPressTime = currentTime;
+            }
+        });
     }
 
     @Override
@@ -52,18 +91,28 @@ public class EarthPart extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && !isJumping) {
             isJumping = true;
-            repaint(); // 화면을 다시 그려줌으로써 변경된 이미지를 표시
+            timer.start();
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            KeyState.isRightPressed = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            KeyState.isLeftPressed = true;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            isJumping = false;
-            repaint(); // 화면을 다시 그려줌으로써 변경된 이미지를 표시
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            KeyState.isRightPressed = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            KeyState.isLeftPressed = false;
         }
+    }
+
+    private static class KeyState {
+        private static boolean isRightPressed = false;
+        private static boolean isLeftPressed = false;
     }
 
     public static void main(String[] args) {
