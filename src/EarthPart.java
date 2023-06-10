b09e7c9382c7f3eb444852c29ae2442d107be367
chaeyoung1027 <s2111@e-mirim.hs.kr>
2023-06-11 03:48:26 +0900

    !!점프, 물체 추가하기 해야한다!

--- src/EarthPart.java
+++ src/EarthPart.java
@@ -5,8 +5,10 @@ import java.awt.event.KeyListener;
 
 public class EarthPart extends JFrame {
     private Image earthBackground;
-    private Image[] rightRabbit = new Image[2];
-    private Image[] leftRabbit = new Image[2];
+    private Image[] rightRabbit = new Image[3];
+    private Image[] leftRabbit = new Image[3];
+    private Image rabbitBetween;
+    private Image leftRabbitBetween;
 
     private boolean isJumping = false;
     private boolean isMovingLeft = false;
@@ -21,7 +23,7 @@ public class EarthPart extends JFrame {
     private int backgroundX = 0; // 배경의 위치
 
     private int frameCount = 0;
-    private int frameDelay = 10; // 0.1초에 한 번씩 변경 (10ms * 16 = 160ms)
+    private int frameDelay = 5; // 토끼 이미지 변경 속도
 
     private int backgroundSpeed = 4; // 배경 이동 속도 : 숫자가 높을수록 빠름
 
@@ -35,11 +37,16 @@ public class EarthPart extends JFrame {
         setVisible(true);
 
         earthBackground = new ImageIcon(getClass().getResource("img/EarthBackground.png")).getImage();
+
         Image rightRabbitRun1 = new ImageIcon(getClass().getResource("img/rabbit_run1.png")).getImage();
+        rabbitBetween = new ImageIcon(getClass().getResource("img/rabbit_between.png")).getImage();
         Image rightRabbitRun2 = new ImageIcon(getClass().getResource("img/rabbit_run2.png")).getImage();
+        Image rightRabbitJump = new ImageIcon(getClass().getResource("img/rabbit_jump.png")).getImage();
 
         Image leftRabbitRun1 = new ImageIcon(getClass().getResource("img/left_rabbit_run1.png")).getImage();
+        leftRabbitBetween = new ImageIcon(getClass().getResource("img/left_rabbit_between.png")).getImage();
         Image leftRabbitRun2 = new ImageIcon(getClass().getResource("img/left_rabbit_run2.png")).getImage();
+        Image leftRabbitJump = new ImageIcon(getClass().getResource("img/left_rabbit_jump.png")).getImage();
 
         int rabbitWidth = rightRabbitRun1.getWidth(null);
         int rabbitHeight = rightRabbitRun1.getHeight(null);
@@ -48,10 +55,12 @@ public class EarthPart extends JFrame {
         scaledHeight = (int) (rabbitHeight * 0.5);
 
         rightRabbit[0] = rightRabbitRun1.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
-        rightRabbit[1] = rightRabbitRun2.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        rightRabbit[1] = rabbitBetween.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        rightRabbit[2] = rightRabbitRun2.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
 
         leftRabbit[0] = leftRabbitRun1.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
-        leftRabbit[1] = leftRabbitRun2.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        leftRabbit[1] = leftRabbitBetween.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        leftRabbit[2] = leftRabbitRun2.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
 
         addKeyListener(new MyKeyListener());
         setContentPane(new MyPanel());
@@ -91,10 +100,10 @@ public class EarthPart extends JFrame {
             // 키 눌림 이벤트 처리
             if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
                 isMovingRight = true;
-                isMovingLeft = false; //오른쪽으로 이동할 때 왼쪽 이동 상태를 해제
+                isMovingLeft = false; // 오른쪽으로 이동할 때 왼쪽 이동 상태를 해제
             } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                isMovingLeft = true; // 추가: 왼쪽으로 이동할 때 왼쪽 이동 상태를 설정합니다.
-                isMovingRight = false; //왼쪽으로 이동할 때 오른쪽 이동 상태를 해제
+                isMovingLeft = true; //왼쪽으로 이동할 때 왼쪽 이동 상태를 설정합니다.
+                isMovingRight = false; // 왼쪽으로 이동할 때 오른쪽 이동 상태를 해제
             }
         }
 
@@ -110,16 +119,23 @@ public class EarthPart extends JFrame {
     }
 
     public void update() {
-        boolean isMoving = isMovingRight || isMovingLeft; //토끼가 이동 중인지 확인
+        boolean isMoving = isMovingRight || isMovingLeft; // 토끼가 이동 중인지 확인
 
         if (isMoving) {
             if (isMovingRight) {
                 rabbitX += rabbitMoveSpeed;
             } else if (isMovingLeft) {
-                rabbitX -= rabbitMoveSpeed; //왼쪽으로 이동할 때 토끼의 위치를 왼쪽으로 변경합니다.
+                rabbitX -= rabbitMoveSpeed; // 왼쪽으로 이동할 때 토끼의 위치를 왼쪽으로 변경합니다.
             }
         } else {
-            rabbitX-=backgroundSpeed;
+            rabbitX -= backgroundSpeed;
+        }
+
+        //토끼가 화면을 벗어나는지 확인하고 게임 오버 처리
+        int rabbitLeftEdge = rabbitX - 10;
+        int rabbitRightEdge = rabbitX + scaledWidth;
+        if (rabbitLeftEdge + 200 < 0 || rabbitRightEdge > getWidth()) {
+            System.out.println("게임 오버");
         }
 
         frameCount++;
@@ -135,16 +151,24 @@ public class EarthPart extends JFrame {
         if (isMovingRight) {
             Image temp = rightRabbit[0];
             rightRabbit[0] = rightRabbit[1];
-            rightRabbit[1] = temp;
+            rightRabbit[1] = rightRabbit[2];
+            rightRabbit[2] = temp;
         } else if (isMovingLeft) {
             Image temp = leftRabbit[0];
             leftRabbit[0] = leftRabbit[1];
-            leftRabbit[1] = temp;
+            leftRabbit[1] = leftRabbit[2];
+            leftRabbit[2] = temp;
         }
     }
 
     private Image getCurrentRabbitImage() {
-        return isMovingLeft ? leftRabbit[1] : rightRabbit[isMovingRight ? 1 : 0];
+        if (isMovingLeft) {
+            return leftRabbit[1];
+        } else if (isMovingRight) {
+            return rightRabbit[1];
+        } else {
+            return rightRabbit[1];
+        }
     }
 
     public static void main(String[] args) {
@@ -153,7 +177,7 @@ public class EarthPart extends JFrame {
             earthPart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             earthPart.setVisible(true);
 
-            // 추가: 주기적으로 업데이트 메소드 호출
+            //주기적으로 업데이트 메소드 호출
             Timer timer = new Timer(16, e -> earthPart.update());
             timer.start();
         });
