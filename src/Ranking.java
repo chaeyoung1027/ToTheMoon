cd7aa51cabd33eac48b91c04596fd2925c459175
Insori <s2110@e-mirim.hs.kr>
2023-06-18 21:19:38 +0900

    랭킹 화면 이미지 띄우기

--- src/Ranking.java
+++ src/Ranking.java
@@ -1,2 +1,38 @@
-public class Ranking {
+import javax.swing.*;
+import java.awt.*;
+
+public class Ranking extends JFrame {
+    private Image screenImage;
+    private Graphics screenGraphic;
+
+    private Image background = new ImageIcon(getClass().getResource("img/RankingBackground.png")).getImage();
+
+    public Ranking() {
+        setUndecorated(true);
+        setSize(1920, 1080);
+        setVisible(true);
+        setBackground(new Color(0, 0, 0, 0));
+        setLocationRelativeTo(null);
+        setLayout(null);
+    }
+
+    // 그리는 함수
+    public void paint(Graphics g) {
+        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(null);
+        screenImage = createImage(1920, 1080);
+        screenGraphic = screenImage.getGraphics();
+        screenDraw(screenGraphic);
+        g.drawImage(screenImage, 0, 0, null);	// background를 그려줌
+    }
+
+    public void screenDraw(Graphics g) {
+        g.drawImage(background , 0, 0, null);
+        paintComponents(g);
+        this.repaint();	// paint 함수로 돌아감
+    }
+
+    public static void main(String[] args) {
+        new Ranking();
+    }
 }
+
