fbf0c6ebae9cc39835a0101190ed6778633d5c0e
chaeyoung1027 <s2111@e-mirim.hs.kr>
2023-06-19 01:09:31 +0900

    랭킹 띄우기

--- src/ToTheMoonDB.java
+++ src/ToTheMoonDB.java
@@ -1,21 +1,33 @@
 import java.sql.*;
+
 public class ToTheMoonDB {
-    public static void main(String args[]) throws SQLException{
+    public static void loadRanking() {
         String url = "jdbc:mysql://localhost:3306/ToTheMoon";
         String userName = "root";
         String password = "mirim";
 
-        Connection connection = DriverManager.getConnection(url, userName, password);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from ranking");
+        String rankingText = ""; // 랭킹 정보를 저장할 변수
 
-        while(resultSet.next()){
-            String score = resultSet.getString("score");
-            String playerID = resultSet.getString("playerID");
-            System.out.println(score+" "+playerID);
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ranking");
+
+            int rank = 1;
+            while(resultSet.next()){
+                String score = resultSet.getString("score");
+                String playerID = resultSet.getString("playerID");
+                rankingText += rank + "           " + playerID + "      " + score + "\n";
+                rank++;
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        resultSet.close();
-        statement.close();
-        connection.close();
+
+        Ranking ranking = new Ranking();
+        ranking.setRankingText(rankingText); // 랭킹 정보를 Ranking 클래스에 설정
     }
 }
